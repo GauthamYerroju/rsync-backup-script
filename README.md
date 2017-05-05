b67812c488add5f5d339e39b06a64093c9d6d733
Gautham Yerroju <gautham.yerroju@gmail.com>
2017-05-05 00:05:02 -0700

    Added commandline options, perform checks of src and dest paths, dry run by default

--- README.md
+++ README.md
@@ -6,32 +6,60 @@ Simple script to mirror relevant folders in home directory to external drive. Th
 - NTFS-compatible (back up to external HDD, also for use in Windows)
 - Portable script with least possible dependencies
 
-I found a [great article on using rsync with NTFS drives](https://ubuntuforums.org/showthread.php?t=820425). With that as the starting point and some Googling, I came up with this script. Some "features":
+I found a [great article on using rsync with NTFS drives](https://ubuntuforums.org/showthread.php?t=820425). With that as the starting point and some Googling, I came up with this script.
+
+## Features
 
 - Runs off of just rsync and basic bash commands, no other dependencies
 - Runs without sudo, for current user
-- Within destination directory, creates separate folders for each hostname+username+userID (in case multiple users back up to the same destination, and for easy identification after OS reinstalls)
+- Safe: only does a dry run by default
+- Within destination directory, creates separate folders for each hostname+username+userID (in case multiple users back up to the same destination, and for easy identification after OS reinstallation)
+- Detailed logs saved in backup folder
 - Simple file-based backup for easy restore
 - NTFS compatible
-- Simple old version and deleted file backups
+- Backups old versions and deleted files
   - Delete backups older than 30 days
+
+## Options
+
+### 1. Source (-s)
+
+The source path. __Optional__.
+- Defaults to current user's home directory
+- Should be an absolute path
+- Should not be a symlink
+
+Mind the trailing slash (refer to how rsync handles trailing slashes).
+
+### 2. Destination (-d)
+
+The destination path __Mandatory__.
+- Should be an absolute path
+- Should not be a symlink
+
+A new folder will be created within the destination path, of the form: `hostname_username-linuxUserId`. For example: `gautham-laptop_gautham-1000`
+
+### 3. Confirm (-c)
+
+Performs the actual backup. __Optional__. Running without -c will do a dry run.
+
+## Need to know before using
+
+- Created for local use, not tested with any type of remote destinations
+- Does not account for paths being symlinks
+- Assumes that paths are absolute
 
 ## Usage
 
 1. Clone repo
 2. Edit filter.txt
-3. Change source and destination directories in script
-4. Edit script to add --dry-run flag and test out (thoroughly!!)
-5. Remove --dry-run and run script
-6. Optionally, add it as a cron job with reasonable frequency
-
-## Notes
-
-- Created for local use, not tested with any type of remote destinations
+3. Do a dry run: `bash rsyncHome.sh -s "{source_path}" -d "{destination_path}"`
+4. Do an actual run by adding -c: `bash rsyncHome.sh -s "{source_path}" -d "{destination_path}" -c`
+5. Optionally, add it as a cron job with preferred frequency
 
 ## TODO:
 
-- Commandline option for dry-running
-- Commandline option for source and dest (and possibly other params)
 - Some solution to backup encrypted files
 - Create non-backup 2-way variant for Music directory
+- DONE ~~Commandline option for dry-running~~
+- DONE ~~Commandline option for source and dest (and possibly other params)~~
--- rsyncHome.sh
+++ rsyncHome.sh
@@ -1,25 +1,113 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +%Y%m%d%H%M%S)
-# Paths relating to script location (to find filter and log locations)
-PWD="`pwd`"
-FILTERFILE="$PWD/filter.txt"
+# Set initial defaults for parameters
+SOURCE="/"
+DESTINATION="/"
+REAL_RUN="no"
 
-SOURCE="/home/$USER/"
-# Calculate destination based on current PC (hostname) and username
-DESTINATION="/media/gautham/Seagate Backup Plus Drive/rsync-backups/$(hostname)_$USER-`id -u $USER`"
+# Check if commandline parameters are passed. If so, set variables
+while getopts s:d:c opts; do
+    case $opts in
+        s)
+            SOURCE="$OPTARG"
+            ;;
+        d)
+            DESTINATION="$OPTARG"
+            ;;
+        c)
+            REAL_RUN="yes"
+            ;;
+    esac
+done
+
+# Exit if destination is not provided
+if [ "$DESTINATION" == "/" ]; then
+    echo "Destination needs to be provided: -d \"destination_path\""
+    exit 1
+fi
+
+# Default to current user's' home directory if no source path is specified
+if [ "$SOURCE" == "/" ]; then
+    SOURCE="/home/$USER/"
+    echo "Source path not provided. Using home directory: \"$SOURCE\""
+    echo
+fi
+
+# TODO Symlink check instructions: https://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shell-script
+# TODO Canonicalize paths instructions (for relative path support): https://stackoverflow.com/questions/32857392/bash-check-if-a-relative-path-exists
+
+# Check if source directory exists
+if [ ! -d "$SOURCE" ]; then
+    echo "Directory \"$SOURCE\" does not exist"
+    exit 1
+fi
+
+# Check if source directory is readable
+if [ ! -r "$SOURCE" ]; then
+    echo "Directory \"$SOURCE\" is not readable"
+    exit 1
+fi
+
+# Check if destination directory exists
+if [ ! -d "$DESTINATION" ]; then
+    echo "Directory \"$DESTINATION\" does not exist"
+    exit 1
+fi
+
+# Check if destination directory is writable
+if [ ! -r "$DESTINATION" ]; then
+    echo "Directory \"$DESTINATION\" is not writable"
+    exit 1
+fi
+
+# Append current PC (hostname) and username to destination
+DESTINATION="${DESTINATION%/}/$(hostname)_$USER-`id -u $USER`"
+
+# Set logfile location in the destination
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
 LOGFILE="$DESTINATION/.logs/rsync.$TIMESTAMP.log"
 mkdir -p "$DESTINATION/.logs"
 
-rsync -rltDuPv --progress --human-readable --modify-window=1 --log-file="$LOGFILE" \
---delete --backup --backup-dir="$DESTINATION/.backups" --suffix=".$TIMESTAMP.backup" \
---partial --partial-dir="$DESTINATION/.partials" \
---filter="merge $FILTERFILE" --filter=":- .gitignore" \
---exclude="$DESTINATION/.backups" --exclude="$DESTINATION/.logs" \
-"$SOURCE" "$DESTINATION"
+FILTERFILE="`pwd`/filter.txt"
 
-# Clean files older than 30 days
-find "$DESTINATION/.backups/" -daystart -mtime +30 -type f -delete
+do_rsync() {
+    rsync -rltDuPv --progress --human-readable --modify-window=1 --log-file="$LOGFILE" \
+    --delete --backup --backup-dir="$DESTINATION/.backups" --suffix=".$TIMESTAMP.backup" \
+    --partial --partial-dir="$DESTINATION/.partials" \
+    --filter="merge $FILTERFILE" --filter=":- .gitignore" \
+    --exclude="$DESTINATION/.backups" --exclude="$DESTINATION/.logs" \
+    "$SOURCE" "$DESTINATION"
+
+    # Clean files older than 30 days
+    find "$DESTINATION/.backups/" -daystart -mtime +30 -type f -delete
+}
+
+do_rsync_dry_run() {
+    rsync -rltDuPv --progress --dry-run --human-readable --modify-window=1 --log-file="$LOGFILE" \
+    --delete --backup --backup-dir="$DESTINATION/.backups" --suffix=".$TIMESTAMP.backup" \
+    --partial --partial-dir="$DESTINATION/.partials" \
+    --filter="merge $FILTERFILE" --filter=":- .gitignore" \
+    --exclude="$DESTINATION/.backups" --exclude="$DESTINATION/.logs" \
+    "$SOURCE" "$DESTINATION"
+
+    # Clean files older than 30 days
+    find "$DESTINATION/.backups/" -daystart -mtime +30 -type f -delete
+}
+
+echo "Source:       $SOURCE"
+echo "Destination:  $DESTINATION"
+echo
+if [ $REAL_RUN == "yes" ]; then
+    echo "Starting backup..."
+    echo
+    do_rsync
+else
+    echo "Starting dry run..."
+    echo
+    do_rsync_dry_run
+    echo
+    echo "Run command with -c to backup"
+fi
 
 # TODO
 # - Music 2-way sync
